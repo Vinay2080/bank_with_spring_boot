9c29c36a029bde36b0dc5afb3534c92242eebb7d
Vinay2080 <shaking.121@gmail.com>
2025-08-31 23:16:57 +0530

    added files to git ignore

---
+++ src/main/java/org/example/bankwithspringboot/BankWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bankwithspringboot/BankWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
